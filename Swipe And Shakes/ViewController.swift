1b91a24355c452cfaac081af16194692ce549930
Rupu <rupuislamashraful7@gmail.com>
2020-11-17 23:22:47 +0600

    Swipe and Shakes

--- Swipe And Shakes/ViewController.swift
+++ Swipe And Shakes/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let swifpeRight = UISwipeGestureRecognizer(target: self, action: #selector(swipeGesture(gesture:)))
+        swifpeRight.direction = UISwipeGestureRecognizer.Direction.right
+        self.view.addGestureRecognizer(swifpeRight)
+        
+        let swifpeLeft = UISwipeGestureRecognizer(target: self, action: #selector(swipeGesture(gesture:)))
+        swifpeLeft.direction = UISwipeGestureRecognizer.Direction.left
+        self.view.addGestureRecognizer(swifpeLeft)
+        
     }
 
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        
+        if event?.subtype == UIEvent.EventSubtype.motionShake {
+            print("shake that booty")
+        }
+    }
+    
+    @objc func swipeGesture(gesture: UIGestureRecognizer) {
+        if let swipegesture = gesture as? UISwipeGestureRecognizer {
+            switch swipegesture.direction {
+            case UISwipeGestureRecognizer.Direction.right :
+                print("Swipe Right")
+                
+            case UISwipeGestureRecognizer.Direction.left :
+                print("Swipe Left")
+            default:
+                break
+            }
+        }
+    }
 
 }
 
